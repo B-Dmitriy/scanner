0f4b5833f77f68c7c7a91174b39cfa60041edb53
B-Dmitriy <balyaevds.main@gmail.com>
2024-04-06 17:52:27 +0300

    fix: add main page styles

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -1,17 +1,34 @@
-<header>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid d-flex ">
-            <a href="/">
-                <img src="/public/icons/logo.svg" alt="logo" width="40" height="40"/>
-                <span >Record shop</span>
-            </a>
-
-            <div class="d-flex">
-                <a class="m-1 btn btn-primary" href="/">Главная</a>
-                <a class="m-1 btn btn-primary" href="/about">О нас</a>
-                <a class="m-1 btn btn-primary" href="/card">Корзина</a>
-                <a class="m-1 btn btn-primary" href="/user">Войти</a>
-            </div>
+<header class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a href="/">
+            <img src="/public/icons/logo.svg" alt="logo" width="40" height="40"/>
+            <span >Record shop</span>
+        </a>
+        <button 
+            class="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNavDropdown" 
+            aria-controls="navbarNavDropdown" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+        >
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Каталог</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/about">О нас</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/card">Корзина</a>
+            </li>
+        </ul>
         </div>
+    </div>
     </nav>
 </header>
--- app/views/pages/main.php
+++ app/views/pages/main.php
@@ -8,7 +8,7 @@
 <?php include_once __DIR__ . '/../layout/head.php'?>
 <body>
 <?php include_once __DIR__ . '/../layout/header.php'?>
-<main>
+<main class="container">
     <section>
         <section class="p-4">
             <h3>Добро пожаловать в Records shop!</h3>
@@ -17,26 +17,50 @@
             <button class="btn btn-primary">Зарегестрироваться</button>
         </section>
 
-        <section style="border: red 1px solid;">
-            Search
-        </section>
-
-        <section style="border: red 1px solid;">
-            Filters
-        </section>
-
-        <section style="border: red 1px solid;">
-            <ul>
+        <nav class="navbar navbar-light bg-light pb-2">
+            <div class="container-fluid">
+                <form class="d-flex mb-2">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success me-2" type="submit">Search</button>
+                </form>
+                <ul class="pagination d-flex mb-2">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container mt-4">
+            <div class="row">
                 <?php foreach($data as $record){ ?>
-                <li>
-                    <b><?= $record['name'] ?></b>
-                    <span><?= $record['price'] ?>$</span>
-                </li>
+                    <div class="col d-flex justify-content-center pb-4">
+                        <section class="card" style="width: 18rem;">
+                            <img src="https://media.istockphoto.com/id/690066030/photo/red-and-white-siamese-fighting-fish-half-moon-shape-isolated-on-black.jpg?s=612x612&w=0&k=20&c=mFDtldxcFbeC70-feOem0nUjNu8MVHBTwo-2aXIwMUM=" class="card-img-top" alt="logo">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $record['name'] ?></h5>
+                                <p class="card-text"><?= $record['price'] ?>$</p>
+                                <p class="card-text"><?= $record['description'] ?></p>
+                                <button href="#" class="btn btn-primary">В корзину</button>
+                                <a href="#" class="btn btn-link">Подробнее...</a>
+                            </div>
+                        </section>
+                    </div>
                 <?php } ?>
-            </ul>
-        </section>
+            </div>
+        </div>
     </section>
 </main>
 <?php include_once __DIR__ . '/../layout/footer.php'?>
+<script src="../../../public/js/bootstrap.min.js"></script>
 </body>
 </html>
